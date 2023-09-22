181dfda1d06ce097ebd03c65d7cfe06c2ca5b20d
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-09-22 23:42:39 +0530

    Update Ec2_creation.tf

--- Ec2_creation.tf
+++ Ec2_creation.tf
@@ -10,3 +10,38 @@ resource "aws_instance" "foo" {
       Name = "ec2-TFjenkins-Instance"
   }
 }
+
+# an empty resource block
+resource "null_resource" "name" {
+
+  # ssh into the ec2 instance 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("/home/ec2-user/PEM/autojen_inst_in_ec2.pem")
+    host        = aws_instance.foo.public_ip
+  }
+
+  # copy the install_jenkins.sh file from your computer to the ec2 instance 
+  provisioner "file" {
+    source      = "/home/ec2-user/Scripts/install_jenkins.sh"
+    destination = "/tmp/install_jenkins.sh"
+  }
+
+  # set permissions and run the install_jenkins.sh file
+  provisioner "remote-exec" {
+    inline = [
+        "sudo chmod +x /tmp/install_jenkins.sh",
+        "sh /tmp/install_jenkins.sh",
+    ]
+  }
+
+  # wait for ec2 to be created
+  depends_on = [aws_instance.foo]
+}
+
+
+# print the url of the jenkins server
+output "website_url" {
+  value     = join ("", ["http://", aws_instance.foo.public_dns, ":", "8080"])
+}
