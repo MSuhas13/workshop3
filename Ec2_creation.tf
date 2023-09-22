d9aebd32bc48164864cc2ef6aa18e7b5026772b3
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-09-22 19:45:47 +0530

    Update Ec2_creation.tf

--- Ec2_creation.tf
+++ Ec2_creation.tf
@@ -43,5 +43,5 @@ resource "null_resource" "name" {
 
 # print the url of the jenkins server
 output "website_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+  value     = join ("", ["http://", aws_instance.foo.public_dns, ":", "8080"])
 }
