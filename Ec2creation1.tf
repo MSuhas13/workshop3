9fbf245432dfec278ebfa3c82cc22942a1a8249a
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-09-23 23:00:16 +0530

    Create Ec2creation1.tf

---
+++ Ec2creation1.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region ="us-east-1"
+}
+
+resource "aws_instance" "webserver" {
+  ami ="ami-002829755fa238bfa"
+  instance_type = "t2.medium"
+  key_name = "autojen_inst_in_ec2"
+  associate_public_ip_address = true
+
+   provisioner "file" {
+        source      = "install_jenkins.sh"
+        destination = "/home/ec2-user/install_jenkins.sh"
+       
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file("/home/ec2-user/PEM/autojen_inst_in_ec2.pem")}"
+      host        = "${self.public_ip}"
+    }
+   }
+
+
+  tags = {
+    Name = "FileProvisionerDemo"
+  }
+}
