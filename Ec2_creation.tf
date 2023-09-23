9639b86f75ab2328c9f6d63ef565a2bfdf1247f3
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-09-23 22:15:35 +0530

    Update Ec2_creation.tf

--- Ec2_creation.tf
+++ Ec2_creation.tf
@@ -24,8 +24,8 @@ resource "null_resource" "name" {
   #echo "----------------ssh done-------------"
   # copy the install_jenkins.sh file from your computer to the ec2 instance 
   provisioner "file" {
-    source      = "/home/ec2-user/Scripts/install_jenkins.sh"
-    destination = "/tmp/install_jenkins.sh"
+    source      = file("/home/ec2-user/Scripts/install_jenkins.sh")
+    destination = file("/tmp/install_jenkins.sh")
   }
   #echo "----------copy install jenkins file done--------------"
   #echo "--------install jenkins file execution in progress--------"
