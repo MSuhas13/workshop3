407741db62066402015d79c7a87e789f8497c73b
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-10-02 20:08:30 +0530

    Update Ec2creation.tf

--- Ec2creation.tf
+++ Ec2creation.tf
@@ -1,27 +1,47 @@
 provider "aws" {
-  region ="us-west-2"
+    region = "us-west-2"  
 }
 
-resource "aws_instance" "webserver" {
-  ami ="ami-002829755fa238bfa"
-  instance_type = "t2.medium"
-  key_name = "autojen_inst_in_ec2"
-  associate_public_ip_address = true
-
-   provisioner "file" {
-        source      = "/home/ec2-user/Scripts/install_jenkins.sh"
-        destination = "/tmp/install_jenkins.sh"
-       
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = "${file("/home/ec2-user/PEM/autojen_inst_in_ec2.pem")}"
-      host        = "${self.public_ip}"
-    }
-   }
-
-
+resource "aws_instance" "foo" {
+  ami           = "ami-002829755fa238bfa" # us-east-1
+  instance_type = "t2.micro"
+  key_name      = "autojen_inst_in_ec2"
   tags = {
-    Name = "FileProvisionerDemo"
+      Name = "ec2-TFjenkins-Instance"
   }
+}
+
+# an empty resource block
+resource "null_resource" "name" {
+
+  # ssh into the ec2 instance 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("/home/ec2-user/PEM/autojen_inst_in_ec2.pem")
+    host        = aws_instance.foo.public_ip
+  }
+
+  # copy the install_jenkins.sh file from your computer to the ec2 instance 
+  provisioner "file" {
+    source      = "/home/ec2-user/Scripts/install_jenkins.sh"
+    destination = "/tmp/install_jenkins.sh"
+  }
+
+  # set permissions and run the install_jenkins.sh file
+  provisioner "remote-exec" {
+    inline = [
+        "sudo chmod +x /tmp/install_jenkins.sh",
+        "sh /tmp/install_jenkins.sh",
+    ]
+  }
+
+  # wait for ec2 to be created
+  depends_on = [aws_instance.foo]
+}
+
+
+# print the url of the jenkins server
+output "website_url" {
+  value     = join ("", ["http://", aws_instance.foo.public_dns, ":", "8080"])
 }
