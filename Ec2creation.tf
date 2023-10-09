34232cc1f2e1a69c14f399a3594d44e340ee7a8c
velayutham19931208 <138760043+velayutham19931208@users.noreply.github.com>
2023-10-09 12:21:26 +0530

    Update Ec2creation.tf

--- Ec2creation.tf
+++ Ec2creation.tf
@@ -2,9 +2,47 @@ provider "aws" {
     region = "us-west-2"  
 }
 
+# create security group for the ec2 instance
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
+  description = "allow access on ports 8080 and 22"
+  vpc_id      = "vpc-0f7433f017285a686"
+
+  # allow access on port 8080
+  ingress {
+    description      = "http proxy access"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  # allow access on port 22
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "jenkins server security group"
+  }
+}
+
+## launch the ec2 instance
 resource "aws_instance" "foo" {
   ami           = "ami-002829755fa238bfa" # us-east-1
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["aws_security_group.ec2_security_group.id"]
   key_name      = "autojen_inst_in_ec2"
   tags = {
       Name = "ec2-TFjenkins-Instance"
